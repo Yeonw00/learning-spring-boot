bb50cfe618455bad15d49767d832f52441365bb0
Yeonw00 <121034815+Yeonw00@users.noreply.github.com>
2024-08-12 17:54:25 +0900

    Delete ReadBook100Ton directory

--- ReadBook100Ton/배지영_성명건_강연우/OrderByAuthor.java
+++
@@ -1,8 +0,0 @@
-import java.util.Comparator;
-
-public class OrderByAuthor implements Comparator<Book>{
-	@Override
-	public int compare(Book book1, Book book2){
-		return book1.getAuthor().compareTo(book2.getAuthor());
-	}
-}
--- ReadBook100Ton/배지영_성명건_강연우/OrderByGrade.java
+++
@@ -1,8 +0,0 @@
-import java.util.Comparator;
-
-public class OrderByGrade implements Comparator<Book> {
-	@Override
-	public int compare(Book book1, Book book2){
-		return book1.getGrade() - book2.getGrade();
-	}
-}
